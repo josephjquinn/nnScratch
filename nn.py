3780db92faaef3c00dd92160a1fcd9f0b01ec3c9
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-06-11 10:09:33 -0400

    initial nn class

--- nn.py
+++ nn.py
@@ -1,6 +1,6 @@
 import numpy as np
-from act import ReLU
-from act import softmax
+from activation import ReLU
+from activation import softmax
 
 
 class nn:
@@ -12,6 +12,7 @@ class nn:
         self.W2 = np.random.rand(10, 10) - 0.5
         self.b1 = np.random.rand(10, 1) - 0.5
         self.b2 = np.random.rand(10, 1) - 0.5
+        self.learning_rate = 0.01
 
     def forward_prop(self, X):
         Z1 = self.W1.dot(X) + self.b1
@@ -20,16 +21,47 @@ class nn:
         A2 = softmax(Z2)
         return Z1, A1, Z2, A2
 
-    def predict(self, X):
-        _, _, _, A2 = self.forward_prop(X)
-        predictions = np.argmax(A2, axis=0)
-        return predictions
+    def one_hot(self, Y):
+        one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+        one_hot_Y[np.arange(Y.size), Y] = 1
+        one_hot_Y = one_hot_Y.T
+        return one_hot_Y
 
-    def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    def backward_prop(self, Z1, A1, Z2, A2, W1, W2, X, Y):
+        m = X.shape[1]
+
+        dZ2 = A2 - Y
+        dW2 = (1 / m) * np.dot(dZ2, A1.T)
+        db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+        dZ1 = np.dot(W2.T, dZ2) * (A1 > 0)
+        dW1 = (1 / m) * np.dot(dZ1, X.T)
+        db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+
+        return dW1, db1, dW2, db2
+
+    def optimize(self, dW1, db1, dW2, db2):
+        self.W1 = self.W1 - self.alpha * dW1
+        self.b1 = self.b1 - self.alpha * db1
+        self.W2 = self.W2 - self.alpha * dW2
+        self.b2 = self.b2 - self.alpha * db2
+
+    def train(self, X, val, epochs):
         pass
 
+    def predict(self, X):
+        _, _, _, A2 = self.forward_prop(X)
+        pred = np.argmax(A2, axis=0)
+        return pred
 
-net = nn()
-print(net.W1)
-print(net.W1.shape)
-print(net.W1.shape)
+    def get_predictions(self, A2):
+        return np.argmax(A2, axis=0)
+
+    def make_predictions(self, X):
+        _, _, _, A2 = self.forward_prop(X)
+        predictions = self.get_predictions(A2)
+        return predictions
+
+    def get_accuracy(self, X, Y):
+        predictions = self.make_predictions(X)
+        accuracy = np.mean(predictions == Y)
+        return accuracy
--- test_nn.py
+++ test_nn.py
@@ -3,31 +3,53 @@ import pytest
 from nn import nn
 
 
-@pytest.fixture
-def nn_instance():
+@pytest.fixture(scope="module")
+def neural_network():
     return nn()
 
 
-def test_initialize_parameters(nn_instance):
-    assert nn_instance.W1.shape == (10, 784)
-    assert nn_instance.W2.shape == (10, 10)
-    assert nn_instance.B1.shape == (10,)
-    assert nn_instance.B2.shape == (10,)
-    assert np.all(nn_instance.W1 >= -0.5) and np.all(nn_instance.W1 <= 0.5)
-    assert np.all(nn_instance.W2 >= -0.5) and np.all(nn_instance.W2 <= 0.5)
-    assert np.all(nn_instance.B1 >= -0.5) and np.all(nn_instance.B1 <= 0.5)
-    assert np.all(nn_instance.B2 >= -0.5) and np.all(nn_instance.B2 <= 0.5)
+def test_initialize_parameters(neural_network):
+    assert neural_network.W1.shape == (10, 784)
+    assert neural_network.W2.shape == (10, 10)
+    assert neural_network.b1.shape == (10, 1)
+    assert neural_network.b2.shape == (10, 1)
 
 
-def test_forward_prop(nn_instance):
-    X = np.random.rand(784)
-    Z1, A1, Z2, A2 = nn_instance.forward_prop(X)
+def test_forward_propagation(neural_network):
+    X = np.random.rand(784, 1)
 
-    assert Z1.shape == (10,)
-    assert A1.shape == (10,)
-    assert Z2.shape == (10,)
-    assert A2.shape == (10,)
+    Z1, A1, Z2, A2 = neural_network.forward_prop(X)
 
-    assert np.all(A1 >= 0)
+    assert Z1.shape == (10, 1)
+    assert A1.shape == (10, 1)
+    assert Z2.shape == (10, 1)
+    assert A2.shape == (10, 1)
 
-    assert np.isclose(np.sum(A2), 1)
+
+def test_back_prop(neural_network):
+    X = np.random.rand(784, 1)
+    Y = np.random.rand(10, 1)
+
+    Z1, A1, Z2, A2 = neural_network.forward_prop(X)
+
+    dW1, db1, dW2, db2 = neural_network.backward_prop(
+        Z1, A1, Z2, A2, neural_network.W1, neural_network.W2, X, Y
+    )
+
+    assert dW1.shape == (10, 784)
+    assert db1.shape == (10, 1)
+    assert dW2.shape == (10, 10)
+    assert db2.shape == (10, 1)
+
+
+def test_one_hot(neural_network):
+    Y = np.random.randint(low=0, high=10, size=125)
+    one_hot_y = neural_network.one_hot(Y)
+    print(one_hot_y.shape)
+    assert one_hot_y.shape == (10, 125)
+
+
+def test_predict(neural_network):
+    X = np.random.rand(784, 1)
+    pred = neural_network.predict(X)
+    assert pred.shape == (1,)
