82c47e2ca477575cb24243ae08ba13913bd11872
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-06-13 11:15:05 -0400

    adding leakyrelu act fn

--- activation.py
+++ activation.py
@@ -1,22 +1,26 @@
 import numpy as np
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+def sigmoid(Z, deriv=False):
+    if deriv:
+        return sigmoid(Z) * (1 - sigmoid(Z))
+    else:
+        return 1 / (1 + np.exp(-Z))
 
 
-def sigmoid_deriv(x):
-    return sigmoid(x) * (1 - sigmoid(x))
-
-
-def ReLU(Z):
-    return np.maximum(Z, 0)
-
-
-def ReLU_deriv(z):
-    return z > 0
+def ReLU(Z, deriv=False):
+    if deriv:
+        return Z > 0
+    else:
+        return np.maximum(Z, 0)
 
 
 def softmax(Z):
     exp = np.exp(Z - np.max(Z))
     return exp / exp.sum(axis=0)
+
+
+def leaky_relu(Z, deriv=False):
+    if deriv:
+        return np.where(Z > 0, 1, 0.01)
+    return np.where(Z > 0, Z, 0.01 * Z)
