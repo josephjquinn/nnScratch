38a23639e4c8761491d0c396fba3f4e68fe090d0
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-06-14 09:44:57 -0400

    adjusting nn parameters

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from nn import nn
 from data import DataProcessor
 
-processor = DataProcessor("./MNIST.csv")
+processor = DataProcessor("./fashion-mnist_train.csv")
 processor.split_data()
 processor.get_features_and_labels()
 processor.print_shapes()
@@ -10,19 +10,17 @@ net = nn(10, "relu", "He")
 
 
 # net.predict(processor.x_train, 45)
-# net.predict_grid(processor.x_train, 3)
-test_accuracy = net.get_accuracy(processor.x_test, processor.y_test)
-print(f"Test Accuracy: {test_accuracy * 100:.2f}%")
+# net.predict_grid(processor.x_test, 3)
 
 net.train(
     processor.x_train,
     processor.y_train,
     processor.x_dev,
     processor.y_dev,
-    # mini_batch=True,
-    alpha=0.06,
-    # batch_size=10,
-    epochs=20,
+    alpha=0.1,
+    mini_batch=True,
+    batch_size=100,
+    epochs=10,
     animate=True,
 )
 
@@ -30,5 +28,6 @@ test_accuracy = net.get_accuracy(processor.x_test, processor.y_test)
 
 print(f"Test Accuracy after training: {test_accuracy * 100:.2f}%")
 
+net.predict_grid(processor.x_test, 3)
 # net.predict_grid(processor.x_train, 3)
 # net.predict(processor.x_train, 45)
